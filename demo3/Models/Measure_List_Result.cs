d0c8f70baa10c4044bd4898b3339cf8be858caca
xinyugao777 <35090201+XinyuGaoUmich@users.noreply.github.com>
2018-05-24 12:55:03 -0400

    add create method and required field

--- demo3/Models/Measure_List_Result.cs
+++ demo3/Models/Measure_List_Result.cs
@@ -18,6 +18,7 @@ namespace demo3.Models
         public int Measure_ID { get; set; }
 
         [DisplayName("Measure Abbreviation")]
+        [Required(ErrorMessage = "Please enter the measure abbreviation")]
         public string Measure_Abbreviation { get; set; }
 
         [DisplayName("Measure Title")]
